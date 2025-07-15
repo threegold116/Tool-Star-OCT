1c3882338be7db5a2c9f6d503db8ce909ce3c31f
THREEGOLD <sxjiang@ir.hit.edu.cn>
2025-07-15 20:43:28 +0800

    add sft

--- LLaMA-Factory-main/examples/train_full/train_sft.sh
+++ LLaMA-Factory-main/examples/train_full/train_sft.sh
@@ -1,8 +1,4 @@
-
-
-# module load cuda/12.1.1
-cd ./LLaMA-Factory-main
-CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli train /LLaMA-Factory-main/examples/train_full/qwen_sft_tool_star.yaml
+CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train //share/home/sxjiang/myproject/Tool-Star-OCT/LLaMA-Factory/examples/train_full/qwen_sft_tool_star.yaml
 
 
 
---
+++ LLaMA-Factory-main/train_sft.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli train //share/home/sxjiang/myproject/Tool-Star-OCT/LLaMA-Factory-main/examples/train_full/qwen_sft_tool_star.yaml
+
+
+
